23d7398da6369a1fe31825bb979dc3770c92588f
Asanza Zambrano <dasanza@eservicios.indra.es>
2023-06-17 03:26:07 +0200

    modificar seccion compania

--- src/app/components/compania/compania.component.ts
+++ src/app/components/compania/compania.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-compania',
   templateUrl: './compania.component.html',
   styleUrls: ['./compania.component.css']
 })
-export class CompaniaComponent {
+export class CompaniaComponent implements OnInit {
+  width: string = "";
 
+  ngOnInit() {
+    this.onResize();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    let vw = window.innerWidth;
+
+    if (vw < 350) {
+      this.width = '200';
+    }
+    else if (vw < 640){
+      this.width = '300';
+    } 
+    else if (vw < 1024) {
+      this.width = '450';
+    } else {
+      this.width = '500';
+    }
+  }
 }
